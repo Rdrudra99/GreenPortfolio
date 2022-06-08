1f064f4c27098e5a4b0d2104f05f09421ff17432
Rudra Narayan Boitei <rudra2021@gift.edu.in>
2022-06-08 16:00:41 +0530

    wow

--- js/index.js
+++ js/index.js
@@ -1,73 +1,21 @@
+const body = document.body;
+let lastScroll = 0;
 
-window.addEventListener('DOMContentLoaded', event => {
-
-  // Navbar shrink function
-  var navbarShrink = function () {
-      const navbarCollapsible = document.body.querySelector('#mainNav');
-      if (!navbarCollapsible) {
-          return;
-      }
-      if (window.scrollY === 0) {
-          navbarCollapsible.classList.remove('navbar-shrink')
-      } else {
-          navbarCollapsible.classList.add('navbar-shrink')
-      }
-
-  };
-
-  // Shrink the navbar 
-  navbarShrink();
-
-  // Shrink the navbar when page is scrolled
-  document.addEventListener('scroll', navbarShrink);
-
-  // Activate Bootstrap scrollspy on the main nav element
-  const mainNav = document.body.querySelector('#mainNav');
-  if (mainNav) {
-      new bootstrap.ScrollSpy(document.body, {
-          target: '#mainNav',
-          offset: 72,
-      });
-  };
-
-  // Collapse responsive navbar when toggler is visible
-  const navbarToggler = document.body.querySelector('.navbar-toggler');
-  const responsiveNavItems = [].slice.call(
-      document.querySelectorAll('#navbarResponsive .nav-link')
-  );
-  responsiveNavItems.map(function (responsiveNavItem) {
-      responsiveNavItem.addEventListener('click', () => {
-          if (window.getComputedStyle(navbarToggler).display !== 'none') {
-              navbarToggler.click();
-          }
-      });
-  });
-
-});
-
-
-
-
-
-// -----------------------------------------
-// const body = document.body;
-// let lastScroll = 0;
-
-// window.addEventListener('scroll', () => {
-//   const currentScroll = window.pageYOffset
-//   if (currentScroll <= 0) {
-//     body.classList.remove("scroll-up")
-//   }
-//   if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
-//     body.classList.remove("scroll-up")
-//     body.classList.add("scroll-down")
-//   }
+window.addEventListener('scroll', () => {
+  const currentScroll = window.pageYOffset
+  if (currentScroll <= 0) {
+    body.classList.remove("scroll-up")
+  }
+  if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+    body.classList.remove("scroll-up")
+    body.classList.add("scroll-down")
+  }
   
-//   if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
-//     body.classList.remove("scroll-down")
-//     body.classList.add("scroll-up")
-//   }
+  if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
+    body.classList.remove("scroll-down")
+    body.classList.add("scroll-up")
+  }
 
 
-//   lastScroll = currentScroll;
-// })
+  lastScroll = currentScroll;
+})
