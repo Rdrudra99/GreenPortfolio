becef4d79faf5cff00d4b210cb72a70ad63f8826
Rudra Narayan Boitei <rudra2021@gift.edu.in>
2022-06-09 08:44:26 +0530

    nabvar

--- js/index.js
+++ js/index.js
@@ -19,3 +19,11 @@ window.addEventListener('scroll', () => {
 
   lastScroll = currentScroll;
 })
+
+// -------
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
